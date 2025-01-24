5072c37bb93f31ad771bb4352cee194d7b79fa97
Grephon2512 <grephon777@gmail.com>
2025-01-24 11:56:39 +0530

    Add client and server directories

---
+++ client/src/components/ColorPicker.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { SketchPicker } from "react-color";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+import { color } from "framer-motion";
+
+const ColorPicker = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.color}
+        disabledAlpha
+        presetColors={[
+          "#007BFF",
+          "#FF007F",
+          "#FF4500",
+          "#C0A25C",
+          "#F8F7F3",
+          "#708238",
+          "#D4B483",
+          "#C48B8B",
+          "#D4A017",
+        ]}
+        onChange={(color) => (state.color = color.hex)}
+      />
+    </div>
+  );
+};
+
+export default ColorPicker;
